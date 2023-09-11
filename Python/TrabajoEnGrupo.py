1652945e68cd7ef6dfe45404a6b1241c30aa9ad1
Nicolas <nicolasezequielgutierrez@gmail.com>
2023-09-11 20:41:42 -0300

    Ejercicios de pyton 1 semestre

---
+++ Python/TrabajoEnGrupo.py
@@ -0,0 +1,35 @@
+"""Califica tu dia del 1 al 10(Ejercicio1)"""
+"""
+print("Hola Nicolas,Buenos dias")
+dia=int(input("Califica tu dia  del 1 al 10:"))
+print("Mi dia estuvo de",dia)
+"""
+"""
+Ejercicio 2: 
+Se solicita incluir la siguiente información acerca de un libro:
+título
+autor
+Debes imprimir la información en el siguiente formato:
+Proporciona el título:
+Proporciona el autor:
+<título> fue escrito por <autor>
+"""
+"""
+titulo=input("Proporciona el titulo:")
+autor=input("Proporciona el autor:")
+print(titulo,"Fue escrito por",autor)
+"""
+"""
+num=int(input("Ingresar numero :"))
+if num%2==0:
+    print("El numero ingresado es par")
+else:
+    print("El numero ingresado es impar")
+"""
+"""
+edad=int(input("Ingrese edad:"))
+if edad >18:
+    print("Es mayor de edad")
+else:
+    print("Es menor de edad")
+"""
