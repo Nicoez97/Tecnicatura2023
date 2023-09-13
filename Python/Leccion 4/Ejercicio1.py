a4d7619600171fbacf513f8b155e58082e259c89
Nicolas <nicolasezequielgutierrez@gmail.com>
2023-09-13 18:13:44 -0300

    Clase 4,Ejercicio 1

---
+++ Python/Leccion 4/Ejercicio1.py
@@ -0,0 +1,12 @@
+#Ejercicio 1 : Llenar una lista
+#Llenar una lista con los numeros del 1 al 50,luego mostrar
+#la lsita con el buble for, por elementos deben mostrarse
+#de la siguiente forma:
+# 1-2-3-4-5.....-50
+lista=[]
+i=1
+while i <=50:
+    lista.append(i)
+    i += 1
+for i in lista:
+    print(i,end='-')
