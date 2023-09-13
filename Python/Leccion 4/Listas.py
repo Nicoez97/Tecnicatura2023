0b4e53416cdb537e044c62532b3bf566ba7cfbd2
Nicolas <nicolasezequielgutierrez@gmail.com>
2023-09-12 21:22:40 -0300

    Clase 3,Tarea de Diccionario

--- Python/Leccion 4/Listas.py
+++ Python/Leccion 4/Listas.py
@@ -254,7 +254,11 @@ seleccionArgentina = {
     11: {'Nombre':'Angel Di Maria','Edad': 34, 'Altura': 1.80, 'Precio':'12 Millones', 'Posicion':'Extremo Derecho'},
     24: {'Nombre':'Paulo Dybala','Edad':28,'Altura':1.77,'Precio':'35 Millones','Posicion':'Media Punta'},
     19: {'Nombre':'Nicolás Otamendi','Edad':34,'Altura':1.83,'Precio':'3.5 Millones','Posicion':'Defensa Central'},
-    1: {'Nombre':'Franco Armani','Edad':35,'Altura':1.89,'Precio':'3.5 Millones','Posicion':'Portero'}
+    1: {'Nombre':'Franco Armani','Edad':35,'Altura':1.89,'Precio':'3.5 Millones','Posicion':'Portero'},
+    6: {'Nombre': 'Leandro Paredes', 'Edad': 29, 'Altura': 1.82, 'Precio': '12 Millones', 'Posicion': 'Centrocampista'},
+    21: {'Nombre': 'Alexis McAllister', 'Edad': 24, 'Altura': 1.76, 'Precio': '40 Millones', 'Posicion': 'Centrocampista'},
+    25: {'Nombre': 'Enzo Fernandez', 'Edad': 22, 'Altura': 1.78, 'Precio': '121 Millones', 'Posicion': 'Centrocampista'},
+    8: {'Nombre': 'Rodrigo De Paul', 'Edad': 29, 'Altura': 1.80, 'Precio': '37 Millones', 'Posicion': 'Centrocampista'}
 }
 for llave, valor  in seleccionArgentina.items():
     print(llave,valor)
