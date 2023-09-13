f1ef9c115cbbfebed6889c8de251eff2949c4f8e
Nicolas <nicolasezequielgutierrez@gmail.com>
2023-09-12 21:19:20 -0300

    Clase 3,Repaso Diccionario2

--- Python/Leccion 4/Listas.py
+++ Python/Leccion 4/Listas.py
@@ -249,3 +249,16 @@ print(diccionarioNuevo)
 diccionario2 ={'Ariel':{'Edad':40,'Altura':1.83},'Osvaldo':[45,1.85],'Natalia':[35,1.67]}
 print(diccionario2)
 
+seleccionArgentina = {
+    10: {'Nombre':'Lionel Messi','Edad':35,'Altura':1.70,'Precio':'50 Millones','Posicion':'Extremo Derecho'},
+    11: {'Nombre':'Angel Di Maria','Edad': 34, 'Altura': 1.80, 'Precio':'12 Millones', 'Posicion':'Extremo Derecho'},
+    24: {'Nombre':'Paulo Dybala','Edad':28,'Altura':1.77,'Precio':'35 Millones','Posicion':'Media Punta'},
+    19: {'Nombre':'Nicolás Otamendi','Edad':34,'Altura':1.83,'Precio':'3.5 Millones','Posicion':'Defensa Central'},
+    1: {'Nombre':'Franco Armani','Edad':35,'Altura':1.89,'Precio':'3.5 Millones','Posicion':'Portero'}
+}
+for llave, valor  in seleccionArgentina.items():
+    print(llave,valor)
+#Como tarea agregar por lo menos 4 jugadores mas al diccionario : seleccionArgentina
+print('Tenemos cargados en el diccionario la cantidad de jugadores: ',end=' ')
+print(len(seleccionArgentina))#
+
