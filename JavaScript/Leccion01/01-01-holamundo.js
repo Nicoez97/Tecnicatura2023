cdc2a08d8b05c61ff128cdd57e3ed4d1676a94c1
Nicolas <nicolasezequielgutierrez@gmail.com>
2023-09-18 17:13:17 -0300

    JacaScript,TiposDatos

--- JavaScript/Leccion01/01-01-holamundo.js
+++ JavaScript/Leccion01/01-01-holamundo.js
@@ -3,4 +3,3 @@ var nombre = "Ariel";
 nombre="Alberto \nAriel\n";
 var apellido = "Betancud";
 console.log(nombre+apellido);
-
---
+++ JavaScript/Leccion02/02-01-TiposDatos.js
@@ -0,0 +1,6 @@
+//Tipos de datos de javascrips
+/*
+La sintaxis en los comentarios
+es muy similiar a la de Java
+realmente diriamos identica
+*/
