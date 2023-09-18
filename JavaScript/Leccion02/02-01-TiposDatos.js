e06b3e241139e05f3c4c594d8226c13cfeb56f22
Nicolas <nicolasezequielgutierrez@gmail.com>
2023-09-18 17:28:55 -0300

    JacaScript,TiposDatos(srt,num,obj)

--- JavaScript/Leccion02/02-01-TiposDatos.js
+++ JavaScript/Leccion02/02-01-TiposDatos.js
@@ -4,3 +4,16 @@ La sintaxis en los comentarios
 es muy similiar a la de Java
 realmente diriamos identica
 */
+var nombre= "Ariel"; //Tipo str
+console.log(nombre);
+
+var numerico = 3000;//Tipo numerico
+console.log(numerico);
+
+var objeto={
+    nombre :'Ariel',
+    apellido : 'Betacund',
+    telefono : '261254538'
+
+}
+console.log(objeto);
