3289af4fcd807c07b35164bc7455ff162a206fe6
Nicolas <nicolasezequielgutierrez@gmail.com>
2023-09-20 21:10:10 -0300

    JacaScript,Clase 5

--- JavaScript/Leccion02/02-01-TiposDatos.js
+++ JavaScript/Leccion02/02-01-TiposDatos.js
@@ -43,3 +43,19 @@ class Persona{
 }
 
 console.log(typeof Persona);
+
+//Tipo de dato undefined}
+var x;
+console.log(typeof x);
+
+x=undefined;
+console.log(typeof x);
+
+//null: significa ausencia de valor
+var y = null; //null no es un tipo de dato, pero su origen es objetivo
+console.log(typeof y);
+
+//Tipo de dato array y Empty String
+var autos = ['Citroen','Audi','BMW','Ford'];
+console.log(autos);
+console.log(typeof autos);//Pregunt
