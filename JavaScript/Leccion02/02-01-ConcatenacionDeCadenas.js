5ccdfa7d5580cff5cccf1f327bd5979000ddf3a3
Nicolas <nicolasezequielgutierrez@gmail.com>
2023-09-27 16:59:50 -0300

    JacaScript,ContextoCadena

---
+++ JavaScript/Leccion02/02-01-ConcatenacionDeCadenas.js
@@ -0,0 +1,12 @@
+var nombre = 'Jose';
+var apellido = 'Montes';
+var nombreCompleto =nombre+''+apellido;
+console.log(nombreCompleto);
+var nombreCompleto2 = 'Ariel'+''+'Betancud';
+console.log(nombreCompleto2);
+var juntos = nombre+219;//Lee de izquierda a derecha siguiendo la cadena lee el numero como str
+console.log(juntos);
+juntos=nombre+78+17;
+console.log(juntos);
+juntos=78+17+nombre;
+console.log(juntos);
