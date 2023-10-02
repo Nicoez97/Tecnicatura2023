636864675976c7a827a20e23557ccd641e219497
Nicolas <nicolasezequielgutierrez@gmail.com>
2023-10-02 19:15:36 -0300

    Funciones

--- Python/Leccion 5/Funciones.py
+++ Python/Leccion 5/Funciones.py
@@ -6,3 +6,14 @@ def mi_funcion():#Para identificar a la funcion utilizamos parentesis
 mi_funcion()#Estamos llamando la funcion
 mi_funcion()#Se puede llamar a una funcion N cantidad de veces
 mi_funcion()
+
+#Desempaquetando de listas o list Unpacking
+def show (name, lastName):
+    print(name+""+lastName)
+person=["Ariel","Betancud"]
+show (person[0],person[1])#Pasamos uno por uno los datos de la lista a la función
+show(*person) # Esto es lo mismo que lo anterior pero lo pasamos todo junto
+peron2=("Osvaldo","Giodanini")#desempaquetamos a traves de una tupla
+show(*person2)
+person3={"lastName": "Lucero", "name": "Natalia"}
+show(**person3)
