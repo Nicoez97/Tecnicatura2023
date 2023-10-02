d86afe20061ba061b1616040fdc695b8c69152ea
Nicolas <nicolasezequielgutierrez@gmail.com>
2023-10-02 16:38:48 -0300

    Python,Ejercicio10

--- Python/Leccion 4/Ejercicio10.py
+++ Python/Leccion 4/Ejercicio10.py
@@ -1,29 +1,10 @@
-"""Ejercicio11 : Agenda telefonica
-hacer un programa que simule una agneda de contacto,Crear un
-diccionario donde la clave sea el nombre del usuario y el valor
-sea el telefono,el programa tendra el siguiente menu de opciones:
-        1.Nuevo contacto
-        2.Borrar contacto
-        3.Ver contactos existentes
-        4.salir"""
-
-agenda={}
-while True:
-    print("\t.:MENÚ:.")
-    print("1.Nuevo contacto")
-    print("2.Borrar contacto")
-    print("3.Ver contactos existentes")
-    print("4.Salir")
-    opcion=int(input("Digite una opcion de menu: "))
-    if opcion==1:
-        nombre=input("Digite el nombre del contacto: ")
-        telefono=input("Digite el numero de telefono: ")
-        if nombre not in agenda:
-            agenda[nombre]= telefono
-            print("Contacto agregado exitosamente!")
-        else:
-            print("Este nombre de contacto ya existe")
-    elif opcion==2:
-        nombre=input("Cual es el nombre del contacto:")
-        if nombre in agenda:
-            del(agenda[nombre])
+"""Ejercicio10: No repetir caracteres
+Hcaer un programa que pida una cadena por teclado,Luego
+meter los caracteres en unalista sin repetir caracteres.
+"""
+cadena=input("Digite una cadena: ")
+lista=[]
+for i in cadena:
+    if i not in lista: #Si elcaracter aun no esta en la lista
+        lista.append(i)#Lo agregamos a la lista
+print(f"\nLista de caracteres sin repetir ninguno: \n {lista}")
