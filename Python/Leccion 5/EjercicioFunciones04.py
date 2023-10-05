229e9d132e12ffaef67277a6c3a358c2d3e3f0f0
Nicolas <nicolasezequielgutierrez@gmail.com>
2023-10-05 20:30:26 -0300

    Funciones,Éjer04

---
+++ Python/Leccion 5/EjercicioFunciones04.py
@@ -0,0 +1,7 @@
+"""Ejercicio 4 :Calculadora de Impuestos
+Crear una funcion para calcular el total de un pago incluyendo
+un impuesto aplicado.(IVA)
+Formula: pago_total=pago_sin_impuesto + pago_sin_impuesto * (impuesto/100)
+Proporcione el monto del impuesto:21%
+pago con el impuesto:xxxx"""
+#Creamos la funcion que calcula el total del pago incluyendo el impuesto
