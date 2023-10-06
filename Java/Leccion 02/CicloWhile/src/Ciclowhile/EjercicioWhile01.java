37d27b11a8c5603a345c7d88525f6c52ee89c72f
Nicolas <nicolasezequielgutierrez@gmail.com>
2023-10-06 18:21:31 -0300

    Agregamos ejercicios de Java

---
+++ Java/Leccion 02/CicloWhile/src/Ciclowhile/EjercicioWhile01.java
@@ -0,0 +1,14 @@
+
+package Ciclowhile;
+
+
+public class EjercicioWhile01 {
+    public static void main(String[] args) {
+        var conteo=0; //Inferencia de tipos
+        while(conteo<3){
+            System.out.println("conteo="+conteo);
+            conteo++;//Vamos aumentando en uno la variable
+        
+        }
+    }
+}
