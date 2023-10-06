80a89b4be4a33d1603bf36144a674b2c557a1749
Nicolas <nicolasezequielgutierrez@gmail.com>
2023-10-06 17:15:45 -0300

    Agregamos ejercicios de Java

---
+++ Java/Ejercicio_1/src/ejercicio1/ejercicio1.java
@@ -0,0 +1,24 @@
+
+package ejercicio1;
+
+import java.util.Scanner;
+
+
+public class ejercicio1 {
+    public static void main(String[] args) {
+        Scanner entrada= new Scanner(System .in);
+        System.out.println("Digite el nombre del libro:");
+        String nombreLibro = entrada.nextLine();
+        System.out. println("Digite id del libro:");
+        int idLibro = Integer.parseInt(entrada.nextLine());
+        System.out.println("Digite el precio del libro:");
+        double precioLibro= Double.parseDouble(entrada.nextLine());
+        System.out.println("Confirme si el envio es gratuito: ");
+        boolean envioGratuito= Boolean.parseBoolean (entrada.nextLine());
+        
+        System.out.println(nombreLibro+" #"+idLibro);
+        System.out.println("Precio del libro: $"+precioLibro);
+        System.out.println("El envio gratuito del libro es:"+envioGratuito);
+        
+    }
+}
---
+++ Java/HolaMundo/src/HolaMundo.java
@@ -0,0 +1,19 @@
+
+public class HolaMundo {
+   public static void main(String[] args) {
+       System.out.println("Hola Mundo desde Java");
+       
+       int miVariable = 10;
+       System.out.println(miVariable);
+       miVariable=5;
+       System.out.println(miVariable);
+       String miVariableCadena = "Binevenidos";
+       System.out.println(miVariableCadena);
+       miVariableCadena="Sigamos creciendo";
+       System.out.println(miVariableCadena);
+      
+               
+       
+   
+   }          
+}
