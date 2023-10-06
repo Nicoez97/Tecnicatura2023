988cbb818fab5a56b307032e0f05f9f0dd0643a1
Nicolas <nicolasezequielgutierrez@gmail.com>
2023-10-06 19:58:51 -0300

    Sintaxis de Java

--- Java/Leccion 02/CicloWhile/src/Ciclowhile/EjercicioWhile01.java
+++ Java/Leccion 02/CicloWhile/src/Ciclowhile/EjercicioWhile01.java
@@ -19,7 +19,17 @@ public class EjercicioWhile01 {
         }while(contador<=7);
         
         for(var contando=0 ;contando < 7;contando++ ){
-            System.out.println("contando = " + contando);
+            if(contando % 2 ==0){
+                System.out.println("contando = " + contando);
+                break;
+            }
+        }
+        for(var contando=0 ;contando < 7;contando++ ){
+            if(contando % 2 !=0){
+                continue;//Vamos a la siguiente itereación
+            }
+             System.out.println("contando = " + contando);
         }
     }
+        
 }
