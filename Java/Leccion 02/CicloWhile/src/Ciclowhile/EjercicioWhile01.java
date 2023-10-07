4005fbc4412cfd410fba8b40daaf98a317f42560
Nicolas <nicolasezequielgutierrez@gmail.com>
2023-10-07 12:07:59 -0300

    Labels

--- Java/Leccion 02/CicloWhile/src/Ciclowhile/EjercicioWhile01.java
+++ Java/Leccion 02/CicloWhile/src/Ciclowhile/EjercicioWhile01.java
@@ -16,34 +16,28 @@ public class EjercicioWhile01 {
             System.out.println("contador ="+ contador);
             contador++;
         
+
         }while(contador<=7);
         //uso de las palabras break y continue junto a las etiquetas(labels
-        inicio:
+        
         for(var contando=0 ;contando < 7;contando++ ){
             if(contando % 2 ==0){
                 System.out.println("contando = " + contando);
-                break inicio;
+                break ;
             }
         }
-        //Etiquetas Labels
         
+        inicio:
         for(var contando=0 ;contando < 7;contando++ ){
             if(contando % 2 !=0){
-                continue;//Vamos a la siguiente itereación
+                continue inicio;//Vamos a la siguiente itereación
             }
              System.out.println("contando = " + contando);
-        }
-        
-        //Etiquetas Labels
-        
-        for(var contando=0 ;contando < 7;contando++ ){
-            if(contando % 2 !=0){
-                
-                System.out.println("contando = " + contando);
-            
-            }
-        }     
-        
+        }   
+          
+ 
+         
+      
     }
         
 }
