4582607e8d1d39d81d2563297a9485b835d09b32
Nicolas <nicolasezequielgutierrez@gmail.com>
2023-10-09 19:24:20 -0300

    Ejer05JO

--- Java/Leccion 02/EjercicioCiclos05/src/Ciclos05/Ejercicio05.java
+++ Java/Leccion 02/EjercicioCiclos05/src/Ciclos05/Ejercicio05.java
@@ -8,28 +8,26 @@ el número de Intentos hechos.
  */
 package Ciclos05;
 
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
-public class Ciclos05 {
+public class Ejercicio05 {
     public static void main(String[] args) {
-        Scanner entrada=new Scanner(System.in);
         int numero,aleatorio, conteo=0;
         aleatorio=(int)(Math.random()*100);//Esto genera un número aleatorio
         do{
-            System.out.println("Digite un número: ");
-            numero= Integer.parseInt(entrada.nextLine());
+            numero= Integer.parseInt(JOptionPane.showInputDialog("Digite un número: "));
             if(numero<aleatorio){
-                System.out.println("Digite un número mayor");
+                JOptionPane.showConfirmDialog(null,"Digite un número mayor");
             
             }
             else if(numero>aleatorio){
-                System.out.println("Digite un número menor");
+                JOptionPane.showConfirmDialog(null,"Digite un número menor");
             }
             else{
-                System.out.println("\t¡¡¡FELICIDADES!!! Has adivinado el número"); 
+                JOptionPane.showMessageDialog(null,"¡¡¡FELICIDADES!!! Has adivinado el número"); 
             }
             conteo++;
         }while(numero != aleatorio);
-        System.out.println("\tAdivinaste el número en: "+conteo+" intentos");
+        JOptionPane.showConfirmDialog(null,"Adivinaste el número en: "+conteo+" intentos");
     }
 }
