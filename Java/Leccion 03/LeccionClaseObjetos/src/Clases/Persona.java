d28f4a3985e944f474c3505f89210681d67e54f2
Nicolas <nicolasezequielgutierrez@gmail.com>
2023-10-09 21:19:44 -0300

    PruebaPersona

--- Java/Leccion 03/LeccionClaseObjetos/src/Clases/Persona.java
+++ Java/Leccion 03/LeccionClaseObjetos/src/Clases/Persona.java
@@ -6,4 +6,9 @@ public class Persona {
     String nombre;
     String apellido;
     
+    //Métodos de la clase(Acciones)
+    public void obtenerInformacion(){
+        System.out.println("Nombre: "+nombre);
+        System.out.println("Apellido: "+apellido);
+    }
 }
--- Java/Leccion 03/LeccionClaseObjetos/src/Clases/PruebaPersona.java
+++ Java/Leccion 03/LeccionClaseObjetos/src/Clases/PruebaPersona.java
@@ -1,14 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Clases;
 
-/**
- *
- * @author Nicolas
- */
 public class PruebaPersona {
-    
+    public static void main(String[] args) {
+        Persona persona1= new Persona();//Llamamos al constructor
+        persona1.nombre="Ariel";//El valor hexadecimal normalmente comienza con 0x
+        persona1.apellido="Betanncud";
+        persona1.obtenerInformacion();
+        
+        Persona persona2=new Persona();
+        System.out.println("persona2="+persona2);
+        System.out.println("persona1= "+persona1);
+        persona2.obtenerInformacion();
+    }
 }
