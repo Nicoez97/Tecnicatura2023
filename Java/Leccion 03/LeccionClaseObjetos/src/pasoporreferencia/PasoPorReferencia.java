59f994ca7684547a9de1060542bbb4f010190880
Nicolas <nicolasezequielgutierrez@gmail.com>
2023-10-24 20:09:13 -0300

    Return y null

--- Java/Leccion 03/LeccionClaseObjetos/src/pasoporreferencia/PasoPorReferencia.java
+++ Java/Leccion 03/LeccionClaseObjetos/src/pasoporreferencia/PasoPorReferencia.java
@@ -10,10 +10,24 @@ public class PasoPorReferencia {
         System.out.println("persona1 nombre = " + persona1.nombre);
         cambiarValor(persona1);
         System.out.println("El cambio que hicimos en el nombre es: "+persona1.nombre);
-        
+        persona1 = cambiarElValor(persona1);
+        Persona persona2=new Persona();
+        persona2=cambiarElValor(persona2);
     }
     public static void cambiarValor(Persona persona){//parametro por referencia
         persona.nombre="Maria";
+       
+    }
+    public static Persona cambiarElValor(Persona persona){
+        if(persona==null){
+            System.out.println("Valor de persona es invalido :  Null");
+            return null;
+        }
+        else{
+            persona.nombre="Monica";
+        }
+        persona.nombre="Monica";
+        return persona;
     }
     
 }
