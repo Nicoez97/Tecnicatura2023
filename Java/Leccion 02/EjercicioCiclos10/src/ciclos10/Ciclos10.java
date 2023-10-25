1b59ac8075b6d42f8161ec0c92e2087e1d68ad6b
Nicolas <nicolasezequielgutierrez@gmail.com>
2023-10-25 16:07:01 -0300

    Ejer10 con JOptionPane

--- Java/Leccion 02/EjercicioCiclos10/src/ciclos10/Ciclos10.java
+++ Java/Leccion 02/EjercicioCiclos10/src/ciclos10/Ciclos10.java
@@ -1,14 +1,23 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+Ejercicio 10:Pedir 10 números y escribir la suma 
+total
+Hacerlo con la clase Scanner y Joptionpane
+*/
 package ciclos10;
 
-/**
- *
- * @author Nicolas
- */
+import java.util.Scanner;
+
 public class Ciclos10 {
+    public static void main(String[] args) {
+        Scanner entrada=new Scanner(System.in);
+        int numero,suma=0;
+        for(int i = 1;i <=10; i++){
+            System.out.println("Digite un número: ");
+            numero=Integer.parseInt(entrada.nextLine());
+            suma +=numero;
+        }
+        System.out.println("\nLa suma de todos los números es: "+suma);
+        
+    }
     
 }
--- Java/Leccion 03/LeccionClaseObjetos/src/pasoporreferencia/PasoPorReferencia.java
+++ Java/Leccion 03/LeccionClaseObjetos/src/pasoporreferencia/PasoPorReferencia.java
@@ -12,7 +12,9 @@ public class PasoPorReferencia {
         System.out.println("El cambio que hicimos en el nombre es: "+persona1.nombre);
         persona1 = cambiarElValor(persona1);
         Persona persona2=new Persona();
+        //Persona persona2=null;
         persona2=cambiarElValor(persona2);
+        //System.out.println("El nuevo valor del objeto es :"+persona2.nombre);
     }
     public static void cambiarValor(Persona persona){//parametro por referencia
         persona.nombre="Maria";
